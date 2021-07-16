14f4d61ee4fe5fec0d41ab7b4085df06103d6a04
System Administrator <root@jiuqianyedeMac-mini.local>
2021-07-16 11:30:20 +0800

    Site updated: 2021-07-16 11:30:20

--- js/matery.js
+++ js/matery.js
@@ -649,3 +649,76 @@ $(function() {
             $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
         }
     });
+
+    // 初始化加载 tooltipped.
+    $('.tooltipped').tooltip();
+
+
+    Date.prototype.Format = function(fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+    L2Dwidget.init({
+        "model": {
+            jsonPath: "https://cdn.jsdelivr.net/gh/xiazeyu/live2d-widget-models/packages/live2d-widget-model-tororo/assets/tororo.model.json",
+            "scale": 1
+        },
+        "display": {
+            "position": "left",
+            "width": 150,
+            "height": 300,
+            "hOffset": 0,
+            "vOffset": -20
+        },
+        "mobile": {
+            "show": false,
+            "scale": 0.5
+        },
+        "react": {
+            "opacityDefault": 0.85,
+            "opacityOnHover": 0.2
+        }
+    });
+});
+
+function switchNightMode() {
+    $('<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"></div></div>').appendTo($("body")), setTimeout(
+        function() {
+            (!$("body").hasClass("DarkMode")) ? ($("body").addClass("DarkMode"), localStorage.setItem('isDark', '1'), $('#changeMode-top').removeClass("fa-moon").addClass("fa-sun"), $('#modeicon').attr("xlink:href", "#icon-sun")) : ($("body").removeClass(
+                "DarkMode"), localStorage.setItem('isDark', '0'), $('#changeMode-top').removeClass("fa-sun").addClass("fa-moon"), $('#modeicon').attr("xlink:href", "#icon-moon")), setTimeout(function() {
+                $(".Cuteen_DarkSky").fadeOut(1e3, function() {
+                    $(this).remove()
+                })
+            }, 2e3)
+        }), 50
+}
+
+function switchNightModeTop() {
+    if ($("body").hasClass("DarkMode")) {
+        $("body").removeClass("DarkMode");
+        localStorage.setItem('isDark', '0');
+        $('#changeMode-top').removeClass("fa-sun").addClass("fa-moon");
+        $('#modeicon').attr("xlink:href", "#icon-moon");
+    } else {
+        $("body").addClass("DarkMode");
+        localStorage.setItem('isDark', '1');
+        $('#changeMode-top').removeClass("fa-moon").addClass("fa-sun");
+        $('#modeicon').attr("xlink:href", "#icon-sun");
+    }
+}
+
+
+//粒子背景
