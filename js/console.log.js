1500f941488ae848edcdfbcf2d03043d984f330d
System Administrator <root@jiuqianyedeMac-mini.local>
2021-10-02 16:46:48 +0800

    Site updated: 2021-10-02 16:46:47

---
+++ js/console.log.js
@@ -0,0 +1,5 @@
+// 控制台
+console.log("%c 欢迎光临| 花开九千叶的博客 ","color:#000000;background:linear-gradient(270deg, #FFDEE9 0%, #B5FFFC 100%);padding:8px 15px;border-radius:15px 15px 15px 15px");
+
+console.log("%c huakai.site ","color:#000000;background:linear-gradient(270deg, #FFDEE9 0%, #B5FFFC 100%);padding:8px 15px;border-radius:15px 15px 15px 15px");
+
